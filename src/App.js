44a5657933ec3237f96c20f6aa81806b9a1a17db
francogz <francogz.e@gmail.com>
2023-10-22 12:59:39 -0300

    Push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Routes from "./routes";
-// import { Provider } from "react-redux";
 
 function App() {
     return (
         <div className="App">
-            {/* <Provider store={store}> */}
             <Routes />
-            {/* </Provider> */}
         </div>
     );
 }
--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-// import { connect } from "react-redux";
-
 const PrivateRoutes = (props) => {
     const isAuthenticated = JSON.parse(localStorage.getItem("userData"));
 
     // RETURN
     return isAuthenticated ? props.children : <Navigate to="/login" />;
 };
-
-// const mapStateToProps = (state) => ({
-//     isAuthenticated: !!state?.auth?.user,
-//     user: state?.auth?.user,
-// });
 
 export default PrivateRoutes;
